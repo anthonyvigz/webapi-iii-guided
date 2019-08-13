b3da43b5e0c38ea7c2a7931beae6801634fd3251
anthonyvigz <anthonyvigz@gmail.com>
2019-08-12 20:25:20 -0400

    creating middleware

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express'); // importing a CommonJS module
 const helmet = require('helmet');
+const logger = require('morgan');
 
 const hubsRouter = require('./hubs/hubs-router.js');
 
@@ -12,6 +13,12 @@ server.use(express.json());
 
 server.use('/api/hubs', hubsRouter);
 
+server.use(helmet());
+
+server.use(clockout);
+
+server.use(logger('dev'));
+
 server.get('/', (req, res) => {
   const nameInsert = (req.name) ? ` ${req.name}` : '';
 
@@ -20,5 +27,20 @@ server.get('/', (req, res) => {
     <p>Welcome${nameInsert} to the Lambda Hubs API</p>
     `);
 });
+
+function clockout(req, res, next) {
+  const date = new Date();
+  const seconds = date.getSeconds();
+
+  if (seconds % 3 === 0) {
+    res.status(403).json({ message: "You shall not pass" });
+  } else {
+    next();
+  }
+}
+
+function methodLogger(req, res, next) {
+  console.log(`${req.method} Request`)
+}
 
 module.exports = server;
